782815c47e377374168fc6a52156035160366c2e
GAURANG-21 <gaurang2621@gmail.com>
2024-01-04 23:05:00 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express();
 // const {User} = require('./models/index')
 // const bcrypt = require('bcrypt');
 // const UserRepository = require('./repository/user-repository')
+const UserService = require('./services/user-service')
 
 const prepareAndStartServer = ()=>{
 
@@ -24,6 +25,14 @@ const prepareAndStartServer = ()=>{
         // const userRepository = new UserRepository();
         // const user = await userRepository.getById(2);
         // console.log(user.dataValues)
+
+        const userService = new UserService();
+        // const newToken = userService.createToken({email:"gaurang@gmail.com", id:"1"})
+        // console.log("New token is:",newToken)
+
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhdXJhbmdAZ21haWwuY29tIiwiaWQiOiIxIiwiaWF0IjoxNzA0Mzg5MDY0LCJleHAiOjE3MDQzODkwNjR9.ka-WYutBS-xYAP6cmQ-sdNCycNYQqivWAwaKBY6XUIg'
+        // const response = userService.verifyToken(token)
+        // console.log(response)
     })
 }
 
